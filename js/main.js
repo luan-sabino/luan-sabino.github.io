6932a26c89d7b5aea2ad9c42e9fa39361a0269c7
luan.sabino <luanphy.y@gmail.com>
2021-07-26 22:18:39 -0300

    Mudança do carousel, e refatoração de funções

--- js/main.js
+++ js/main.js
@@ -2,24 +2,6 @@ function getElement(element){
     return document.querySelector(element);
 }
 
-
-
-function createGlider(element){
-        new Glider(document.querySelector(element), {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            draggable: true,
-            dragVelocity: 1.4,
-            scrollLock: true,
-            dots: ".js-carousel--responsive-dots",
-            arrows: {
-              prev: ".js-carousel--responsive-prev",
-              next: ".js-carousel--responsive-next",
-            },
-        });
-      
-}
-
 const portfolioButton = getElement("#portfolio");
 const contactsButton  = getElement("#contacts");
 const aboutButton     = getElement("#about");
@@ -27,50 +9,76 @@ const cardAbout       = getElement(".card-about");
 const cardAboutClass  = cardAbout.className;
 const container       = getElement(".container")
 
-portfolioButton.addEventListener('click', ()=>{
-    cardChange(`
-    <div class="glider-contain">
-                <div class="glider">
-                    <div class="c-carousel__slide">
-                        <a href="https://luan-sabino.github.io/PokeDex/" target="_blank">
-                            <img src="imgs/pokedex.png" alt="">
-                            <p>PokeDex</p>
-                        </a>
-                    </div>
-                    <div class="c-carousel__slide">
-                        <a href="https://luan-sabino.github.io/task-manager/" target="_blank">
-                            <img src="imgs/taskmanager.png" alt="">
-                            <p>Gerenciador de Tarefas</p>
-                        </a>
-                    </div>
-                    <div class="c-carousel__slide">
-                        <a href="https://github.com/luan-sabino/PokeStore" target="_blank">
-                            <img src="imgs/pokeStore.png" alt="">
-                            <p>PokeStore</p>
-                        </a>
-                    </div>
-                    <div class="c-carousel__slide">
-                        <a href="https://luan-sabino.github.io/snake-game/" target="_blank">
-                            <img src="imgs/snakegame.png" alt="">
-                            <p>Jogo da Cobrinha</p>
-                        </a>
-                    </div>
-                    <div class="c-carousel__slide">
-                        <a href="https://github.com/luan-sabino/netflix-frontpage-clone" target="_blank">
-                            <img src="imgs/netflixclone.png" alt="">
-                            <p>Replica Netflix Front-page</p>
-                        </a>
-                    </div>
-                    <div class="c-carousel__slide">
-                        <a href="https://github.com/luan-sabino/instagramcontinuepage" target="_blank">
-                            <img src="imgs/instagramloginclone.png" alt="">
-                            <p>Replica Instagram Login Page </p>
-                        </a>
+const contentCardPortfolio = `
+<div class="owl-carousel owl-theme ">
+                        <div class="item" id="#item1">
+                            <a href="https://luan-sabino.github.io/PokeDex/" target="_blank">
+                                <img src="imgs/pokedex.png" alt="" class="box-img">
+                                <p>PokeDex</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://luan-sabino.github.io/task-manager/" target="_blank">
+                                <img src="imgs/taskmanager.png" alt="" class="box-img">
+                                <p>Gerenciador de Tarefas</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://github.com/luan-sabino/PokeStore" target="_blank">
+                                <img src="imgs/pokeStore.png" alt="" class="box-img">
+                                <p>PokeStore</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://luan-sabino.github.io/snake-game/" target="_blank">
+                                <img src="imgs/snakegame.png" alt="" class="box-img">
+                                <p>Jogo da Cobrinha</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://github.com/luan-sabino/netflix-frontpage-clone" target="_blank">
+                                <img src="imgs/netflixclone.png" alt="" class="box-img">
+                                <p>Replica Netflix Front-page</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="https://github.com/luan-sabino/instagramcontinuepage" target="_blank">
+                                <img src="imgs/instagramloginclone.png" alt="" class="box-img">
+                                <p>Replica Instagram Login Page </p>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-    `, ' c-carousel__slides');
-    createGlider('.glider');
+`
+
+function insertCarousel(){
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        margin:10,
+        dots: true,
+        loop: false,
+        responsiveBaseElement: "#card-about",
+        responsive : {
+            0 : {
+                items:1,
+            },
+            480  : {
+                items:1,
+            },
+            768  : {
+                items:1,
+            },
+            1024 : {
+                items:1,
+            }
+        }
+    })
+}
+
+portfolioButton.addEventListener('click', ()=>{
+    cardChange(contentCardPortfolio);
+    insertCarousel();
+
 });
 
 contactsButton.addEventListener('click', ()=>{
@@ -88,15 +96,15 @@ contactsButton.addEventListener('click', ()=>{
 
 aboutButton.addEventListener('click', ()=>{
     cardChange(
-    `<p>Atualmente sou bacharelando em Ciencia da Computação pela
+    `<p id="aboutMe">Atualmente sou bacharelando em Ciencia da Computação pela
     FCT-UNESP. Tenho 21 anos, sou apaixonado por tecnologia e amo resolver problemas. Gosto muito de
     Desenvolvimento Web e Mobile, e alterno meus estudos entre os conteudos da faculdade, e essas duas areas.</p>`
     );
 });
 
-function cardChange(cardContent, carousel = ''){
+function cardChange(cardContent){
     cardAbout.parentNode.removeChild(cardAbout);
     cardAbout.innerHTML = cardContent;
-    cardAbout.className = cardAboutClass + " component-style show-card" + carousel;
+    cardAbout.className = cardAboutClass + " component-style show-card";
     container.appendChild(cardAbout);
 }
